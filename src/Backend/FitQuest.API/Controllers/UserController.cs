39364dfbf8fbe7792efdbf35e6d5ab24c973f5cd
Guilherme0364 <guilhermedg364@outlook.com>
2025-01-29 19:35:24 -0300

    feat: create user controller, requests and responses

---
+++ src/Backend/FitQuest.API/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using FitQuest.Communication.Requests;
+using FitQuest.Communication.Response;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitQuest.API.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterUserJson request) // 
+        {
+            return Created();
+        }
+    }
+}
---
+++ src/Shared/FitQuest.Communication/Response/ResponseRegisteredUserJson.cs
@@ -0,0 +1,7 @@
+﻿namespace FitQuest.Communication.Response
+{
+    public class ResponseRegisteredUserJson
+    {
+        public string Nome { get; set; }
+    }
+}

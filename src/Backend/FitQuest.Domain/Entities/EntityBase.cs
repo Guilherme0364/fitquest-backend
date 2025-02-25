35ef014d7316f6a20609443577098b202024a84c
Guilherme0364 <guilhermedg364@outlook.com>
2025-02-25 01:39:07 -0300

    feat: add Domain classes

---
+++ src/Backend/FitQuest.Domain/Entities/EntityBase.cs
@@ -0,0 +1,10 @@
+﻿namespace FitQuest.Domain.Entities
+{
+    public class EntityBase
+    {
+        // Todas as entidades vão ter esses atributos via herança
+        public long Id { get; set; }
+        public bool Active { get; set; } = true; // Para não vir false como default
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+    }
+}

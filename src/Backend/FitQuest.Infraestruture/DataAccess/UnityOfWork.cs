3ccdbb63796e52a3fa213f4d3aadb65838c623e2
Guilherme0364 <guilhermedg364@outlook.com>
2025-02-25 01:42:49 -0300

    feat: add Infraestructure classes

---
+++ src/Backend/FitQuest.Infraestruture/DataAccess/UnityOfWork.cs
@@ -0,0 +1,20 @@
+﻿using FitQuest.Domain.Repositories;
+using FitQuest.Infraestruture.DataAccess;
+
+namespace FitQuest.Infraestructure.DataAccess
+{
+    public class UnityOfWork : IUnityOfWork
+    {
+        private readonly FitQuestDbContext _dbContext;
+
+        public UnityOfWork(FitQuestDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Commit()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/Backend/FitQuest.Infraestruture/DependencyInjectionExtension.cs
@@ -0,0 +1,39 @@
+﻿using FitQuest.Domain.Repositories;
+using FitQuest.Domain.Repositories.User;
+using FitQuest.Infraestructure.DataAccess;
+using FitQuest.Infraestruture.DataAccess;
+using FitQuest.Infraestruture.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FitQuest.Infraestructure
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void AddInfraestructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            AddDbContext_SqlServer(services, configuration);
+            AddRepositories(services);
+        }
+
+        private static void AddDbContext_SqlServer(IServiceCollection services, IConfiguration configuration)
+        {
+            // Sinal de exclamação no final para indicar que sabemos que o valor não será nulo
+            string connectionString = configuration.GetConnectionString("ConnectionSqlServer")!;
+
+
+            services.AddDbContext<FitQuestDbContext>(dbContextOptions =>
+            {
+                dbContextOptions.UseSqlServer(connectionString);
+            });
+        }
+
+        private static void AddRepositories(IServiceCollection services)
+        {
+            services.AddScoped<IUnityOfWork, UnityOfWork>();
+            services.AddScoped<IUserWriteOnlyRepository, UserRepository>();
+            services.AddScoped<IUserReadOnlyRepository, UserRepository>();
+        }
+    }
+}

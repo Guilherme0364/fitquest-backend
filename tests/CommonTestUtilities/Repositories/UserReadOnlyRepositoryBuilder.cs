0b2cffbedc41f7d40f9f86908d35f5b48755b80a
Guilherme0364 <guilhermedg364@outlook.com>
2025-04-13 19:06:11 -0300

    feat: add login tests

--- tests/CommonTestUtilities/Repositories/UserReadOnlyRepositoryBuilder.cs
+++ tests/CommonTestUtilities/Repositories/UserReadOnlyRepositoryBuilder.cs
@@ -1,4 +1,5 @@
-﻿using FitQuest.Domain.Repositories.User;
+﻿using FitQuest.Domain.Entities;
+using FitQuest.Domain.Repositories.User;
 using Moq;
 
 namespace CommonTestUtilities.Repositories
@@ -16,6 +17,11 @@ namespace CommonTestUtilities.Repositories
         {
             // Ela da acesso às funções da interface
             _repository.Setup(repository => repository.ExistisActiveUserWithEmail(email)).ReturnsAsync(true);
+        }
+
+        public void GetByEmailAndPassword(User user)
+        {            
+            _repository.Setup(repository => repository.GetByEmailAndPassword(user.Email, user.Password)).ReturnsAsync(user);
         }
 
         public IUserReadOnlyRepository Build()
---
+++ tests/UseCases.Test/Login/DoLogin/DoLoginUseCaseTest.cs
@@ -0,0 +1,42 @@
+﻿using CommonTestUtilities.Cryptography;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.Repositories;
+using FitQuest.Application.UseCases.Login.DoLogin;
+using FitQuest.Communication.Requests;
+using FitQuest.Domain.Entities;
+using FluentAssertions;
+
+namespace UseCases.Test.Login.DoLogin
+{
+    public class DoLoginUseCaseTest
+    {
+        [Fact]
+        public async Task Success()
+        {
+            // Porque a função retorna dois valores
+            (var user, var password) = UserBuilder.Build();
+
+            var useCase = CreateUseCase(user);
+
+            var result = await useCase.Execute(new RequestLoginJson
+            {
+                Email = user.Email,
+                Password = user.Password
+            });
+
+            result.Should().NotBeNull();            
+            result.Name.Should().NotBeNullOrWhiteSpace().And.Be(user.Name);            
+        }
+
+        private static DoLoginUseCase CreateUseCase(FitQuest.Domain.Entities.User? user = null)
+        {
+            var passwordEncrypter = PasswordEncrypterBuilder.Build();
+            var userReadOnlyRepositoryBuilder = new UserReadOnlyRepositoryBuilder();
+
+            if (user is not null)
+                userReadOnlyRepositoryBuilder.GetByEmailAndPassword(user);
+
+            return new DoLoginUseCase(userReadOnlyRepositoryBuilder.Build(), passwordEncrypter);
+        }
+    }
+}
